292a66f8a6924f9c406847f32b0ca8fef20c0b85
Daly2025 <lydiafdez84@gmail.com>
2025-05-23 16:34:49 +0200

    imagen portada

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 <main class="home">
     <section class="welcome">
-        <h1 data-text="stylos">stylos</h1>
+        <h1 data-text="stylos">Stylos</h1>
         <p>Tu salón de belleza de confianza en Vigo donde la elegancia se encuentra con el estilo.</p>
+        <div class="hairstyles-animation">
+            <img src="img/dibus.jpeg" alt="Diferentes estilos de peinados" class="animated-hairstyles">
+        </div>
     </section>
 </main>
 
